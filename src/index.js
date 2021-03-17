3927460a8348cb6a5cefac2ba3b222f228283895
Duong Nguyen <duongf5vcu@gmail.com>
2021-03-17 08:59:49 +0700

    add handlebars template engine

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+// add handlebars
+const exphbs = require('express-handlebars');
+const app = express ();
+const port = 3000;
+
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+
+// app set views
+app.set('view engine','hbs')
+app.set('views',path.join(__dirname,'resources\\views'));
+
+//router
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+app.get('/news',(req,res)=>{
+    res.render('news');
+})
+
+
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`Example app listening at http://localhost:${port}`)
+})
