636ea1efc0a1e5683a23f9b1f6a411740072df8a
Duong <duongf5vcu@gmail.com>
2021-08-06 14:48:00 +0700

    duongvaidai

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/detail', function () {
+    return view('detail');
+});
+
 Route::get('/love', function () {
     return view('new');
 });
